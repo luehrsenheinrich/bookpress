ada575c9f2d511adb54e5228650a90dbfd717e53
Hendrik Luehrsen <hendrik@luehrsen.de>
2013-03-08 18:01:04 +0100

    First changes to the article loop

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php get_header(); ?>
 
-<div class="container">
+<div class="container the_loop">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     
-    	<div <?php post_class("span12 media"); ?>>
-        	<h3 class="media-heading"><?php the_title(); ?></h3>
-        	<div class="media-body"><?php the_content(); ?></div>
-        </div>
+    	<article <?php post_class("span12"); ?>>
+        	<h2 class="headline"><?php the_title(); ?></h2>
+        	<div class="the_content"><?php the_content(); ?></div>
+        	<div class="the_meta">
+        		<div class="infos">
+        			 <?php echo __("by", "lh")." ".get_the_author()." ".__("at")." ".get_the_date(); ?>
+        		</div>
+        		<div class="comments">
+        			<i class="icon-comment">
+        			</i>
+        			<span class="comments_number"><?php comments_number(); ?></span>
+        		</div>
+        	</div>
+        </article>
     
     <?php endwhile; endif; ?>
 </div>
