8d7c75d1c8bd9da879c9201689f8e756400af5f7
Hendrik Luehrsen <hendrik@luehrsen.de>
2013-11-13 16:43:29 +0100

    Fixes

--- sources/lh.facebook.php
+++ sources/lh.facebook.php
@@ -43,7 +43,9 @@ class lh_fb_toolset {
 				var_dump($_POST['signed_request'], $_COOKIE['bp_signed_request'], FB_APP_SECRET);
 		if(isset($_POST['signed_request'])){ // First visit of the user, retrive and store that stuff!
 			$this->signed_request = $this->parse_signed_request($_POST['signed_request'], $this->fb_secret);
-			setcookie('bp_signed_request', $_POST['signed_request']);
+			if($this->signed_request){
+				setcookie('bp_signed_request', $_POST['signed_request']);
+			}
 		} elseif(isset($_COOKIE['bp_signed_request'])){
 			$this->signed_request = $this->parse_signed_request($_COOKIE['bp_signed_request'], $this->fb_secret);
 		} else {
