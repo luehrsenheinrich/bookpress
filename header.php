2e31529d696f7a1e7e7176a9a65f4b6b524a67c8
Hendrik Luehrsen <hendrik@luehrsen.de>
2013-11-14 18:01:18 +0100

    Viewport weg

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title><?php wp_title(); ?></title>
-    <meta name="viewport" content="width=device-width">
     
     <?php wp_head(); ?>
     
